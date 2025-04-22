f21f1ca282d2e6fba808cf40a1d85c3b6385c5d0
anhthuan <zzzzthuan81@gmail.com>
2025-04-22 16:14:41 +0700

    first init commit

---
+++ api/models/user.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const User = sequelize.define('User', {
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    },{
+        timestamps: false,
+        tableName: "user"
+    }
+);
+module.exports = User;
